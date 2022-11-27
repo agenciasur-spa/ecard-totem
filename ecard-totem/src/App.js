7cb735add1526cf4993322fd0deb8165f9dfd2b6
maur-ojeda <49486702+maur-ojeda@users.noreply.github.com>
2022-11-27 17:56:59 -0300

    test

--- ecard-totem/src/App.js
+++ ecard-totem/src/App.js
@@ -1,11 +1,57 @@
 
 import Formulario from './components/Formulario';
 //import logo from './logo.svg';
-//import './App.css';
+import './App.css';
 
 function App() {
   return (
-    <Formulario/>
+
+<div className="container py-5 h-100">
+      <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="col-xl-10">
+          <div className="card rounded-3 text-black shadow-lg ">
+            <div className="row g-0">
+              <div className="col-lg-6">
+                <div className="card-body p-md-5 mx-md-4">
+  
+                  <div className="text-center">
+                    <img src="https://vcard-bestado.web.app/assets/icons/icon-152x152.png" alt="logo" className="logo" />
+                    <h4 className="mt-1 mb-5 pb-1">Solicite su E-card</h4>
+                  </div>
+                  <Formulario/>
+                  </div>
+              </div>
+              <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
+
+
+                <div className="text-white px-3 py-4 p-md-5 mx-md-4">
+                  
+                  <h4 className="mb-4">Instrucciones</h4>
+                  <p className="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+                
+
+
+
+
+                </div>
+
+                
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+ 
   );
 }
 
--- ecard-totem/src/components/Formulario.jsx
+++ ecard-totem/src/components/Formulario.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { useForm } from "react-hook-form";
 import { rutFormato, rutValidator } from "./validators";
+import { QRCodeCanvas } from "qrcode.react";
+
+
+
 
 const Formulario  = () => {
     const [successMsg, setSuccessMsg] = useState("");
-    const [response, setResponse] = useState("response");
+    const [counter, setCounter] = useState(0);
+    const [response, setResponse] = useState(false);
     const {register, formState:{errors}, handleSubmit, reset } = useForm();
     
     const onSubmit = (data) => {
@@ -14,9 +19,29 @@ const Formulario  = () => {
 
         setSuccessMsg("Consulta correcta");
         fetchUsers(data)
+
+
+    const interval =   setInterval(() => {
+            setCounter((prevCounter) => prevCounter + 1);
+          }, 1000);
+      
+
+
+        setTimeout(() => {
+            alert('periodo de inactividad despues de 2 segundos');    
+            setResponse(null)
+            setCounter(0)
+            clearInterval(interval);
+            setSuccessMsg("");
+            reset();    
+          }, 5000);
+
+
         reset();
     };
 
+
+    
 
     const fetchUsers = async rut => {
 		try {
@@ -39,39 +64,51 @@ const Formulario  = () => {
 		}
 	};
 
-    return <>
-        <h2>Formulario</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
+    return(
+
+        <form className="form-inline-" onSubmit={handleSubmit(onSubmit)}>
         
         {successMsg && <p className="success-msg">{successMsg}</p>}
     
         {response && <span>{response[0].name}</span> }
-        {response && <span>{response[0].token}</span> }
-
-    
-        
-       
+        {response ? <QRCodeCanvas id="qrCode" value={`https://stackoverflow.com/questions/39523040/concatenating-variables-and-strings-in-react/pepe=${ response[0].token }`} size={300} bgColor={"#FFFFFF"} level={"H"}/> : ''}
 
 
-            <div>
-                <label>Rut</label>
-                <input type="text" name="rut" {...register('rut', {required: true,  validate: { rutFormato, rutValidator }})}/>
-                {errors.rut?.type === "required" && (
-            <p className="errorMsg">Rut es requerido.</p>
-          )}
-                {errors.rut?.type === "rutFormato" && (<p>Formato no válido.</p>)}
-                {errors.rut?.type === "rutValidator" && (<p>Rut no válido.</p>)}
+   
+        <p>Ingrese su RUN </p>
+
+        <h6 className="text-muted">Tiempo de inactividad: {counter}</h6>
+
+        <div className="form-outline mb-4">
                 
-            </div>  
+                <input className="form-control form-control-lg"  type="text" name="rut" {...register('rut', {required: true,  validate: { rutFormato, rutValidator }})}/>
+                <small id="emailHelp" className="form-text text-muted">Sin puntos y con guión ejemplo: 9999999-9</small>
 
-            <div>
-                <input type="submit" value="Enviar"/>
-            </div>
+                {errors.rut?.type === "required" && (<p className="errorMsg text-danger">Ingreso de Run es requerido.</p>)}
+                {errors.rut?.type === "rutFormato" && (<p className="errorMsg text-danger">El Formato ingresado no es válido.</p>)}
+                {errors.rut?.type === "rutValidator" && (<p>El Run ingresado no es válido.</p>)}
+                
+        </div>  
 
+        
+    
+                   
+  
+                    <div className="text-center pt-1 mb-5 pb-1">
+                      <button className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit">Enviar</button>
+                    
+                        <button  className="btn btn-outline-muted" type="button" onClick={() => setResponse(null)}>Limpiar formulario</button>
 
+                    </div>
+  
+
+                    <div className="d-flex align-items-center justify-content-center pb-4">
+                      
+                      <small>Si tiene problemas Lorem ipsum dolor, sit amet consectetur adipisicing elit. </small>
+                    </div>
+
+        
         </form>
-
-       
-    </>
+)
 }
 export default Formulario;
